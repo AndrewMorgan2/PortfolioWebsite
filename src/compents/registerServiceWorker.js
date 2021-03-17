f475f5cee4ec2181df4a534e35d99267cfc5efb8
AndrewMorgan2 <58256684+AndrewMorgan2@users.noreply.github.com>
2021-03-17 21:01:38 +0000

    Patch v2

--- src/compents/registerServiceWorker.js
+++ src/compents/registerServiceWorker.js
@@ -20,6 +20,7 @@ const isLocalhost = Boolean(
 
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    console.log("worker attempting to register");
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.REACT_APP_API_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
