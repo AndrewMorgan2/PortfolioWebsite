1d3e17e791a760b9f6eedf9a29e6b2293e48b196
AndrewMorgan2 <58256684+AndrewMorgan2@users.noreply.github.com>
2022-03-15 22:11:58 +0000

    Added cloud services

--- src/compents/AboutMe.js
+++ src/compents/AboutMe.js
@@ -11,6 +11,7 @@ export default class AboutMe extends React.Component {
                     I have made this website to display my projects.
                     I'm a full stack developer, I've built my own processor simulator running my own RISC instruction set.
                     I've written projects using basic, Haskell, Prolog, C, C++, C#, go, Java, SQL, HTML and CSS.
+                    I'm also familair with AWS and google cloud.
                     These are some of my projects.
                 </h3>
             </div>
