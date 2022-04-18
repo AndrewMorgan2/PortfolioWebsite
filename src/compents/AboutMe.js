4776f4719213ca0cf1b90c3a46a90c0d70316865
AndrewMorgan2 <andrewdmorgan.2@gmail.com>
2022-04-18 20:53:32 +0100

    Update AboutMe.js

--- src/compents/AboutMe.js
+++ src/compents/AboutMe.js
@@ -11,7 +11,7 @@ export default class AboutMe extends React.Component {
                 <h3 style={{fontSize : "1.5vmax", color: "white"}}>
                     I have made this website to display my projects.
                     I'm a full stack developer, I've built my own processor simulator running my own RISC instruction set.
-                    I've written projects using basic, Haskell, Prolog, C, C++, C#, go, Java, SQL, HTML and CSS.
+                    I've written projects using basic, Haskell, Prolog, C, C++, C#, go, Java, SQL, Python, HTML and CSS.
                     I'm also familair with AWS and google cloud. 
                     
                     These are some of my projects.
