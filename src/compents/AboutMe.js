c223385b181d8f149131655cfe12a4a86267f471
AndrewMorgan2 <andrewdmorgan.2@gmail.com>
2022-04-18 20:47:19 +0100

    Update AboutMe.js

--- src/compents/AboutMe.js
+++ src/compents/AboutMe.js
@@ -5,13 +5,15 @@ export default class AboutMe extends React.Component {
     render(){
         return (
             <div>
-                <br/>
+                <h2>
+                <a href="https://github.com/AndrewMorgan2/">Link to my github</a>
+                </h2>
                 <h3 style={{fontSize : "1.5vmax", color: "white"}}>
                     I have made this website to display my projects.
                     I'm a full stack developer, I've built my own processor simulator running my own RISC instruction set.
                     I've written projects using basic, Haskell, Prolog, C, C++, C#, go, Java, SQL, HTML and CSS.
                     I'm also familair with AWS and google cloud. 
-                    <a href="https://github.com/AndrewMorgan2/">Link to my github</a>
+                    
                     These are some of my projects.
                 </h3>
             </div>
