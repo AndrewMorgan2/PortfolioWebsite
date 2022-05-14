1964d50ab3d41a669d59d1172593726ced895d7c
AndrewMorgan2 <58256684+AndrewMorgan2@users.noreply.github.com>
2022-05-14 11:28:05 +0100

    Update AboutMe.js

--- src/compents/AboutMe.js
+++ src/compents/AboutMe.js
@@ -9,27 +9,27 @@ export default class AboutMe extends React.Component {
                     I have made this website to display my projects.
                     I'm a full stack developer and I've written projects using:
                     <div style={{ display: 'block'}}>
-                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "2vw", }}>
+                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "1vw", }}>
                             <li>basic</li>
                             <li>Haskell</li>
                             <li>Prolog</li>
                         </ul>
-                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "2vw", }}>
+                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "1vw", }}>
                             <li>C</li>
                             <li>C++</li>
                             <li>C#</li>
                         </ul>
-                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "2vw", }}>
+                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "1vw", }}>
                             <li>go</li>
                             <li>Java</li>
                             <li>SQL</li>
                         </ul>
-                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "2vw", }}>
+                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "1vw", }}>
                             <li>Python</li>
                             <li>HTML</li>
                             <li>CSS</li>
                         </ul>
-                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "2vw", }}>
+                        <ul class="list-disc" style={{ display: 'inline-block', marginLeft: "1vw", }}>
                             <li>AWS</li>
                             <li>google cloud</li>
                         </ul>
