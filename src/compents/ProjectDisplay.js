3e267c09d708099d2bb15c82b8a0dab526109410
AndrewMorgan2 <58256684+AndrewMorgan2@users.noreply.github.com>
2022-05-14 11:20:07 +0100

    Update

--- src/compents/ProjectDisplay.js
+++ src/compents/ProjectDisplay.js
@@ -47,7 +47,7 @@ export default function ProjectDisplay() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={DeviceEnergy} style={{ width: '100%', height: '100%' }} />
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Device Energy Modelling</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600 " style={{fontSize: "1.5vmax"}}>Device Energy Modelling</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>
                                         This was a university group project, where we collected energy consumption from a variety of devices and displayed
                                         it in graph form. There is an admin page to edit the data shown. In this project I wrote the database along with parts
@@ -60,7 +60,7 @@ export default function ProjectDisplay() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={BilletSortingSystem} style={{ width: '100%', height: '100%' }} />
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2"style={{fontSize: "1.5vmax"}}>Billet Sorting System</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600 "style={{fontSize: "1.5vmax"}}>Billet Sorting System</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>
                                         This was a team project for Engineering Education Scheme Wales. I lead this project to prove a concept to a
                                         local company. We also presented this project to the customer along with displaying it at a convention.
@@ -72,7 +72,7 @@ export default function ProjectDisplay() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={SolarCharger} style={{ width: '100%', height: '100%' }} />
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Solar Powered Phone Charger</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600 " style={{fontSize: "1.5vmax"}}>Solar Powered Phone Charger</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>
                                         This project is incredably simple, buy some solar panels solder a diode to prevent backflow. Then charge your phone (all be it slowly).
                                         I also had to work out exactly how USB power rails worked but that was about it.
@@ -87,7 +87,7 @@ export default function ProjectDisplay() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={Speaker} style={{ width: '100%', height: '100%' }} />
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Sound to Light Speaker</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600 " style={{fontSize: "1.5vmax"}}>Sound to Light Speaker</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>
                                         The core idea here is to use a microphone to identify 3 different tones. When the corresponding tone is played that colour LED lights up.
                                         This is really satisfying to watch
@@ -99,7 +99,7 @@ export default function ProjectDisplay() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={PinBall} style={{ width: '100%', height: '100%' }} />
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Pinball Machine</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600 " style={{fontSize: "1.5vmax"}}>Pinball Machine</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>
                                         I made this when I was 15, this was my first personal project. The only technical part of this project was the solenoids that provide the force to flippers.
                                         This was actually no easy task, using high voltage is dangerous and I destroyed a few diodes before it worked.
@@ -111,7 +111,7 @@ export default function ProjectDisplay() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={Flask} style={{ width: '100%', height: '100%' }} />
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Self-Heating Flask</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600 " style={{fontSize: "1.5vmax"}}>Self-Heating Flask</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>
                                         So the core idea behind this project was to detect the temperature within the flask, and make it constant. I did this by modifying a flash that I brought, I wrapped chromium wire around the inside of the flash. Then used a thermistor to control when the heating element was active.
                                         I actually wrote the control in basic.
@@ -126,7 +126,7 @@ export default function ProjectDisplay() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={BinaryClock} style={{ width: '100%', height: '100%' }} />
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Binary Clock</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600 " style={{fontSize: "1.5vmax"}}>Binary Clock</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>
                                     The basic concept here is that the top row of LEDs displays the hour and the bottom row displays the minutes. The casing for this project was made out of scrapes that I found around the house,
                                     the log (where the LEDs are housed) was from a tree that feel near my house.
--- src/compents/ShowCase.js
+++ src/compents/ShowCase.js
@@ -41,7 +41,7 @@ export default function ShowCase() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={ProcessorSimulator}  style={{ width: '100%', height: '100%', marginTop:'5%' }}/>
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Processor Simulator</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600" style={{fontSize: "1.5vmax"}}>Processor Simulator</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}>I built a processor with my own instruction set and programmed some simple programs using the simulator.  
                                     <a href="https://github.com/AndrewMorgan2/MyProcessor"> Github Link</a></p>
                                 </div>
@@ -51,7 +51,7 @@ export default function ShowCase() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={robotDog}  style={{ width: '100%', height: '100%'}}/>
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Robot Dog</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600" style={{fontSize: "1.5vmax"}}>Robot Dog</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}> A robot dog that used a ESP32 to set up a wifi network. This wifi network was used to controll the 12 servos on the dog.
                                 Making it remote controlled. </p>
                                 </div>
@@ -61,7 +61,7 @@ export default function ShowCase() {
                             <div class="mx-auto flex w-96 flex-col justify-center bg-white rounded-lg shadow-xl shadow-slate-300/60">
                                 <img class="aspect-video w-96 rounded-t-2xl object-cover object-center rounded-lg" src={Breached}  style={{ width: '100%', height: '100%'}}/>
                                 <div class="p-4">
-                                    <h1 class="text-2xl font-medium text-slate-600 pb-2" style={{fontSize: "1.5vmax"}}>Breached</h1>
+                                    <h1 class="text-2xl font-medium text-slate-600" style={{fontSize: "1.5vmax"}}>Breached</h1>
                                     <p class="text-sm tracking-tight font-light text-slate-400 leading-6" style={{fontSize: "1vmax"}}> First person, web browser co-op escape room style game, complete with water simulation. I did this as a team of 6 in my 3rd year of university
                             Source code: <a href="https://github.com/breached-game/BreachedGame">here's a link to the github page</a></p>
                                 </div>
